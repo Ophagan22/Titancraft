1f18725d4521d6d50981ef02dc4d0df6b75534d9
Ophagan22 <bashstudiorecords@gmail.com>
2024-08-20 19:06:31 +0000

    Mundo guardado 2024-08-20

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ StopAsyncIteration# Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar1
+
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
